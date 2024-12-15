5b08a19c9d2d1a6973ab36ef67e2ebea5c553e6a
lukaszmalinowski14 <lmalinow@N56.pim.ad>
2024-12-15 22:19:31 +0100

    15_12_2024_libft ##final##

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -3,53 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmalinow <lmalinow@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lmalinow <lmalinow@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 20:52:47 by lmalinow          #+#    #+#             */
-/*   Updated: 2024/12/07 13:31:56 by lmalinow         ###   ########.fr       */
+/*   Updated: 2024/12/15 22:07:42 by lmalinow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h> // for size_t
-// #include <stdio.h>
+					// #include <stdio.h>
 
-static int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	while (n > 0 && *s1 && *s2 && *(unsigned char *)s1 == *(unsigned char *)s2)
-	{
-		s1++;
-		s2++;
-		n--;
-	}
-	if (n == 0)
-		return (0);
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
-}
-static size_t ft_strlen(const char *str)
-{
-	int len;
+// static int ft_strncmp(const char *s1, const char *s2, size_t n)
+// {
+// 	while (n > 0 && *s1 && *s2 && *(unsigned char *)s1 == *(unsigned char *)s2)
+// 	{
+// 		s1++;
+// 		s2++;
+// 		n--;
+// 	}
+// 	if (n == 0)
+// 		return (0);
+// 	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+// }
+// static size_t ft_strlen(const char *str)
+// {
+// 	int len;
 
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
+// 	len = 0;
+// 	while (str[len])
+// 		len++;
+// 	return (len);
+// }
 
+// char *ft_strnstr(const char *big, const char *little, size_t len)
+//{
+//  size_t little_len = ft_strlen(little);
+//  size_t i;
+
+// if (little_len == 0)
+// 	return ((char *)big);
+// i = 0;
+// while (i + little_len <= len)
+// {
+// 	if (ft_strncmp(big + i, little, little_len) == 0)
+// 		return (char *)(big + i);
+// 	i++;
+// }
+// return (NULL);
+//}
 char *ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t little_len = ft_strlen(little);
 	size_t i;
+	size_t j;
 
-	if (little_len == 0)
-		return ((char *)big);
+	if (*little == '\0')
+		return (char *)big;
+	if (len == 0)
+		return NULL;
 	i = 0;
-	while (i + little_len <= len)
+	while (big[i] && i < len)
 	{
-		if (ft_strncmp(big + i, little, little_len) == 0)
-			return (char *)(big + i);
+		j = 0;
+		while (big[i + j] == little[j] && i + j < len)
+		{
+			if (little[j + 1] == '\0')
+				return (char *)&big[i];
+			j++;
+		}
 		i++;
 	}
-	return (NULL);
+	return NULL;
 }
 
 // int main(void)
